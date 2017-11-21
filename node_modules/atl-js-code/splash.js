1f9f22a19b79f6dc432e0926ba1724f07377d1d9
angel917k <angel917k@gmail.com>
2017-11-21 10:58:07 -0800

    First Push

---
+++ node_modules/atl-js-code/splash.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+new CircleType(document.getElementById('curve-text-1')).dir(-1).radius(115); //circular text deccoration
+
+$('.navbar-toggle').on('click', function() {
+  $('.after-navbar').css('margin-top','230px');  //scroll do wn entire page on navbar click
+});
+});
